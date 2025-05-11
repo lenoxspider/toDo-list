805d395ef676484d972da1bef13740045936b107
Yoof1337 <dakormedi79@gmail.com>
2025-05-11 21:40:59 +0000

    Update todoList.py

--- todoList.py
+++ todoList.py
@@ -4,7 +4,6 @@ succ = 'Task added.'
 prompt = 'Enter a task for our to-do list. Press <enter> when done: '
 title = 'Your To-Do List:'
 task_list = []
-n = 0
 
 def printer():
     for number in range(0, len(task_list)):
@@ -18,7 +17,6 @@ while True:
         print(title)
         print('-' * len(title))
         printer()
-        print('The number of iterations for the code is', n)
         break
     else:
         task_list.append(task_input)
